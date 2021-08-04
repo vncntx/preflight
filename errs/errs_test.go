4839894768bafd0f9878cfba5f097374a46cc876
Vincent Fiestada <vincent@vincent.click>
2021-08-04 23:43:02 +0800

    test(errs): unit test for Combine

---
+++ errs/errs_test.go
@@ -0,0 +1,20 @@
+package errs_test
+
+import (
+	"errors"
+	"preflight"
+	"preflight/errs"
+	"testing"
+)
+
+func TestCombine(test *testing.T) {
+	t := preflight.Unit(test)
+
+	err1 := errors.New("error 1")
+	err2 := errors.New("error 2")
+	err3 := errors.New("error 3")
+
+	err := errs.Combine(err1, err2, err3)
+
+	t.Expect(err.Error()).Eq("error 1; error 2; error 3")
+}
