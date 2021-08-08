0e1db7b2b19f014477463d187f93dc3597c44ae2
Vincent Fiestada <vincent@vincent.click>
2021-08-08 23:12:32 +0800

    test(stream): unit tests for fault

--- stream/fault_test.go
+++ stream/fault_test.go
@@ -1,7 +1,36 @@
 package stream_test
 
-import "testing"
+import (
+	"errors"
+	"testing"
 
-func TestFaultClose(test *testing.T) {
+	"vincent.click/pkg/preflight"
+	"vincent.click/pkg/preflight/expect"
+	"vincent.click/pkg/preflight/stream"
+)
 
+func TestFaultyClose(test *testing.T) {
+	t := preflight.Unit(test)
+
+	f := stream.Faulty(t.T, errors.New("err"))
+
+	err := f.Close()
+	t.Expect(err).Is().Nil()
+}
+
+func TestFaultyExpectations(test *testing.T) {
+	t := preflight.Unit(test)
+
+	f := stream.Faulty(t.T, errors.New("err"))
+
+	t.Expect(isFaulty(f.Size())).Is().True()
+	t.Expect(isFaulty(f.Text())).Is().True()
+	t.Expect(isFaulty(f.Bytes())).Is().True()
+	t.Expect(isFaulty(f.ContentType())).Is().True()
+}
+
+func isFaulty(e expect.Expectation) bool {
+	_, ok := e.(*expect.Fault)
+
+	return ok
 }
